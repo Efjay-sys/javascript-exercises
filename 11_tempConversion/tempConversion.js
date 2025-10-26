16165acf21028920a219f39ced34554e6a8794eb
Efjay <efjayengelbrecht@gmail.com>
2025-10-26 13:49:40 +0200

    continued with exercises.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+    const celsius = (fahrenheit - 32) * 5 / 9;
+    return celsius;
 };
+const fahrenheitTemp = 68;
+let celsiusTemp = convertToCelsius(fahrenheitTemp)
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(100).toFixed(1))
+
+
+const convertToFahrenheit = function(celsiusValue) {
+  const fahrenheit = (celsiusValue * 9 / 5) + 32;
+  return fahrenheit;
 };
+const celsiusValue = 25;
+const fahrenheitValue = convertToFahrenheit(celsiusValue);
+
+convertToFahrenheit(32);
 
 // Do not edit below this line
 module.exports = {
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count 
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+    if ( count < 0) return "Oops";
+    if ( count == 0) return 0;
 
+    for (let i = 2; i <= count; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+        return firstPrev;
 };
 
 // Do not edit below this line
